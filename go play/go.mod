86a58e00683dbda85e3b79bb52b0fca62e6432c1
Tatyana Yagolnikov <61123875+tatyanayagolnikov@users.noreply.github.com>
2021-08-10 14:28:02 -0700

    Add files via upload

---
+++ go play/go.mod
@@ -0,0 +1,3 @@
+module github.com/tatyanayagolnikov/myniceprogram
+
+go 1.16
---
+++ go play/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tatyanayagolnikov/myniceprogram"
+)
+
+func main() {
+	fmt.Println("Tanya Programmer")
+
+	var myVar SomeType
+	fmt.Println(myVar)
+}
