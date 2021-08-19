4a0a0e401ac0933c87120ad8ef04e5a7126ae1ce
Tatyana Yagolnikov <61123875+tatyanayagolnikov@users.noreply.github.com>
2021-08-18 17:28:38 -0700

    web-server (functions, handlers, error checking) EASY

--- web-server/main.go
+++ web-server/main.go
@@ -1,43 +1,68 @@
 package main
 
 import (
-	"fmt" 
+	"errors"
+	"fmt"
 	"net/http"
 )
+const portNumber = ":8080"
+
+// ----- HANDLER func ----- 
+// In order for a function to respond to a request
+// from a web browser, it has to handle 2 parameters,
+// first- a response writer 
+// second- has to handle a request
+
+// Home - is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About - is the about page HANDLER  
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2,2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page, and 2 + 2 is %d", sum))
+}
+
+func Person(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Tanya Programmer")
+}
+
+// addValues - adds two integers together 
+func addValues(x, y int) int {
+	return x + y 
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return 
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil 
+}
+
 
 func main() {
-
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-	//  n, err:= fmt.Fprintf(w, "Hello, Tanya Programmer")
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
-	// 	fmt.Println(fmt.Sprintf("Number of bytes: %d",n))
-	// })
-
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello, Tanya Programmer, have a good day!")
-	})
-
+	
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/person", Person)
+	http.HandleFunc("/divide", Divide)
+	
+	fmt.Println("starting application on port:", portNumber )
+	fmt.Println(fmt.Sprintf("Starting app on port %s", portNumber),"using fmt.Sprintln")
 	// WEB SERVER that LISTENS for Requests in Go
-	_ = http.ListenAndServe(":8080", nil)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	_ = http.ListenAndServe(portNumber, nil)
 
 }
