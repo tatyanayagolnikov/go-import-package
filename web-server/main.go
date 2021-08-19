f9b47db269acd9f555fbd14822b3cd6c464d50c1
Tatyana Yagolnikov <61123875+tatyanayagolnikov@users.noreply.github.com>
2021-08-18 22:03:08 -0700

    web-server templates html.tmpl
    using the package Emmet on vsc to easily write html pages

--- web-server/main.go
+++ web-server/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
+	"text/template"
 )
 const portNumber = ":8080"
 
@@ -15,41 +15,25 @@ const portNumber = ":8080"
 
 // Home - is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.page.tmpl.html")
 }
 
 // About - is the about page HANDLER  
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2,2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page, and 2 + 2 is %d", sum))
+	renderTemplate(w, "about.page.tmpl.html")
 }
 
 func Person(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Tanya Programmer")
+	renderTemplate(w, "person.page.tmpl.html")
 }
 
-// addValues - adds two integers together 
-func addValues(x, y int) int {
-	return x + y 
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.0, 10.0)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by 0")
+		fmt.Println("error parsing template:", err)
 		return 
 	}
-
-	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0, f))
-}
-
-func divideValues(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil 
 }
 
 
@@ -58,7 +42,7 @@ func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 	http.HandleFunc("/person", Person)
-	http.HandleFunc("/divide", Divide)
+	
 	
 	fmt.Println("starting application on port:", portNumber )
 	fmt.Println(fmt.Sprintf("Starting app on port %s", portNumber),"using fmt.Sprintln")
